34f8f6e7d2b6f216917f37254d86a57a204a6d68
KenshiDRK <KenshiDRK@gmail.com>
2022-02-10 14:27:01 +0100

    Update version
    Update version

--- ApRadar3/My Project/AssemblyInfo.vb
+++ ApRadar3/My Project/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("3.3.0.34")> 
-<Assembly: AssemblyFileVersion("3.3.0.34")> 
+<Assembly: AssemblyVersion("3.3.0.35")> 
+<Assembly: AssemblyFileVersion("3.3.0.35")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en-US")> 
